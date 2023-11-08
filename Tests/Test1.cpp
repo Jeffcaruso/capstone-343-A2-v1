9c81c9a0872b10d63425b2a6a79daef844bf54c5
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-07 23:00:30 -0800

    Test 1,2 partially prepared

--- Tests/Test1.cpp
+++ Tests/Test1.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Test1
-// Test Desc.  : Test that Printing and addition operates as expected
+// Test Desc.  : Test == (basic)
 //				 	(coverage for old test 1 method)
 // Author      : Jeffrey Caruso
 // Date    	   : Fall 2023
@@ -34,32 +34,32 @@ using namespace std;
  * @date ongoing
  */
 
-
 /**
  * Trying to avoid global variables,
  * by creating a singleton class with our visitor functions
  * stringstream SS contains the output from visitor
  */
-class TreeVisitor {
+class TreeVisitor
+{
 public:
-  // never create an instance of TreeVisitor object
-  // we'll just use the static functions
-  TreeVisitor() = delete;
+	// never create an instance of TreeVisitor object
+	// we'll just use the static functions
+	TreeVisitor() = delete;
 
-  // insert output to SS rather than cout, so we can test it
-  static stringstream SS;
+	// insert output to SS rather than cout, so we can test it
+	static stringstream SS;
 
-  // get SS as a string
-  static string getSS() { return SS.str(); }
+	// get SS as a string
+	static string getSS() { return SS.str(); }
 
-  // set SS to be empty string
-  static void resetSS() { SS.str(string()); }
+	// set SS to be empty string
+	static void resetSS() { SS.str(string()); }
 
-  // instead of cout, insert item into SS, a stringstream object
-  static void visitor(const string &Item) { SS << Item; }
+	// instead of cout, insert item into SS, a stringstream object
+	static void visitor(const string &Item) { SS << Item; }
 
-  // instead of cout, insert item into SS, a stringstream object
-  static void visitor(const int &Item) { SS << Item; }
+	// instead of cout, insert item into SS, a stringstream object
+	static void visitor(const int &Item) { SS << Item; }
 };
 
 // initialize the static variable
@@ -69,10 +69,11 @@ public:
 //  NOLINTNEXTLINE
 stringstream TreeVisitor::SS;
 
-template <class T> void visitorSimple(const T &Item) {
-  cout << "visitorSimple: " << Item;
+template <class T>
+void visitorSimple(const T &Item)
+{
+	cout << "visitorSimple: " << Item;
 }
-
 
 // Testing ==
 TEST(Test1, TestEquality)
@@ -82,32 +83,27 @@ TEST(Test1, TestEquality)
 	BST<string> B3;
 
 	EXPECT_TRUE(B1 == B2 && (!(B1 != B2)));
-    B1.add("c");
-    B2.add("c");
-    B3.add("b");
+	B1.add("c");
+	B2.add("c");
+	B3.add("b");
 
-
-
-	  //   assert(B1 == B2 && (!(B1 != B2)));
+	//   assert(B1 == B2 && (!(B1 != B2)));
 	EXPECT_TRUE(B1 == B2 && (!(B1 != B2)));
-  //   assert(B1 != B3 && (!(B1 == B3)));
+	//   assert(B1 != B3 && (!(B1 == B3)));
 	EXPECT_TRUE(B1 != B2 && (!(B1 == B3)));
 
-
-  // cout << "Starting testPisan01" << endl;
-  //   cout << "* Testing == and !=" << endl;
-  //   BST<string> B1;
-  //   BST<string> B2;
-  //   BST<string> B3;
-  //   // == and != for empty trees
-  //   assert(B1 == B2 && (!(B1 != B2)));
-  //   B1.add("c");
-  //   B2.add("c");
-  //   B3.add("b");
-  //   // == and !- for 1-Node trees B1, B2, B3
-  //   assert(B1 == B2 && (!(B1 != B2)));
-  //   assert(B1 != B3 && (!(B1 == B3)));
-  //   cout << "Ending testPisan01" << endl;
+	// cout << "Starting testPisan01" << endl;
+	//   cout << "* Testing == and !=" << endl;
+	//   BST<string> B1;
+	//   BST<string> B2;
+	//   BST<string> B3;
+	//   // == and != for empty trees
+	//   assert(B1 == B2 && (!(B1 != B2)));
+	//   B1.add("c");
+	//   B2.add("c");
+	//   B3.add("b");
+	//   // == and !- for 1-Node trees B1, B2, B3
+	//   assert(B1 == B2 && (!(B1 != B2)));
+	//   assert(B1 != B3 && (!(B1 == B3)));
+	//   cout << "Ending testPisan01" << endl;
 }
-
-//This cannot be easily split without changing the actual tests becuase they depend on each number...
--- Tests/Test2.cpp
+++ Tests/Test2.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Test2
-// Test Desc.  : Test EQ, NE, GT, GTE, LT, LTE comparisons
+// Test Desc.  : Testing detailed == operations
 //				 	(coverage for old test 2 method)
 // Author      : Jeffrey Caruso
 // Date    	   : Fall 2023
@@ -19,45 +19,107 @@ using namespace std;
 // for c strings, use EXPECT_STREQ (or STRNE)
 // http://google.github.io/googletest/reference/assertions.html
 
-TEST(Test2one, CheckEqualAndNE)
-{
-	//Testing Equal and Not Equal with operator overloads
-	//is true that 99 == 99
-	EXPECT_TRUE(LargeNum(99) == LargeNum(99));
-	//is true that 99 != 100
-	EXPECT_TRUE(LargeNum(99) != LargeNum(100));  	
+
+/**
+ * Testing BST - Binary Search Tree functions
+ *
+ * This file has series of tests for BST
+ * Each test is independent and uses assert statements
+ * Test functions are of the form
+ *
+ *      test_netidXX()
+ *
+ * where netid is UW netid and XX is the test number starting from 01
+ *
+ * Test functions can only use the public functions from BST
+ * testBSTAll() is called from main in main.cpp
+ * testBSTAll calls all other functions
+ * @author Multiple
+ * @date ongoing
+ */
+
+
+/**
+ * Trying to avoid global variables,
+ * by creating a singleton class with our visitor functions
+ * stringstream SS contains the output from visitor
+ */
+class TreeVisitor {
+public:
+  // never create an instance of TreeVisitor object
+  // we'll just use the static functions
+  TreeVisitor() = delete;
+
+  // insert output to SS rather than cout, so we can test it
+  static stringstream SS;
+
+  // get SS as a string
+  static string getSS() { return SS.str(); }
+
+  // set SS to be empty string
+  static void resetSS() { SS.str(string()); }
+
+  // instead of cout, insert item into SS, a stringstream object
+  static void visitor(const string &Item) { SS << Item; }
+
+  // instead of cout, insert item into SS, a stringstream object
+  static void visitor(const int &Item) { SS << Item; }
+};
+
+// initialize the static variable
+//  warning: initialization of 'SS' with static storage duration
+//  may throw an exception that cannot be caught [cert-err58-cpp]
+//  Not sure how to do it without making code harder to read
+//  NOLINTNEXTLINE
+stringstream TreeVisitor::SS;
+
+template <class T> void visitorSimple(const T &Item) {
+  cout << "visitorSimple: " << Item;
 }
 
-TEST(TEST2two, CheckGTEandLTE)
+
+
+
+
+//detailed == testing
+TEST(Test2one, DetailedEqualityTesting)
 {
-	//Testing Greater Than or Equal and Less Than or Equal with operator overloads	
-	//is true that 105 >= 100
-	EXPECT_TRUE(LargeNum(105) >= LargeNum(100));
-	//is true that 100 <= 105
-	EXPECT_TRUE(LargeNum(100) <= LargeNum(105));
+	//   cout << "* Testing == and != with more detail" << endl;
+
+	BST<string> B1;
+	BST<string> B2;
+	BST<string> B3;
+
+	//add the following to B1
+	for (auto &S : vector<string>{"c", "a", "f", "g", "x"})
+	{
+		B1.add(S);
+	}
+
+	//add the following to B2
+	for (auto &S : vector<string>{"c", "f", "a", "g", "x"})
+	{
+		B2.add(S);
+	}
+
+	//add "b" to B3
+	B3.add("b");
+
+	// == for the 5 node trees
+	EXPECT_TRUE(B1 == B2 && (!(B1 != B2)));
+
+	//copy constructor usage
+	BST<string> B4(B3);
+	// copy constructor for 1-Node trees B3, B4
+	EXPECT_TRUE(B3 == B4 && (!(B3 != B4)));
+
+	//copy constructor usage
+	BST<string> B5(B1);
+	// copy constructor for 5-Node trees B1, B5
+	EXPECT_TRUE(B1 == B5 && (!(B5 != B1)));
+
+	//copy constructor usage
+	BST<string> B7("b");
+	// 1-param constructor for 1-Node trees B3, B7
+	EXPECT_TRUE(B3 == B7 && (!(B3 != B7)));
 }
-
-TEST(Test2three, CheckGTandLT)
-{
-	//Testing Greater Than and Less Than with operator overloads	
-
-	//is true that 99 > 7
-	EXPECT_TRUE(LargeNum(99) > LargeNum(7));
-	//is true that 7 < 99
-	EXPECT_TRUE(LargeNum(7) < LargeNum(99));
-
-	//is true that 99 > -7
-	EXPECT_TRUE(LargeNum(99) > LargeNum(-7));
-	//is true that -7 < 99
-	EXPECT_TRUE(LargeNum(-7) < LargeNum(99));
-
-	//is true that -5 > -10
-	EXPECT_TRUE(LargeNum(-5) > LargeNum(-10));
-	//is true that -10 < -5
-	EXPECT_TRUE(LargeNum(-10) < LargeNum(-5));
-
-	//is true that (5 > 5 is incorrect)
-	EXPECT_TRUE( ! (LargeNum(5) > LargeNum(5)));
-}
-
-// These tests were feasibly split by category, so students can know and test their inequality operators by type.
