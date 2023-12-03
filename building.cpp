df642d498cb76ba644b69a4664c6f173d5b4d396
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-02 18:40:55 -0800

    lets see what happens with a 1 item bst

--- building.cpp
+++ building.cpp
@@ -6,7 +6,7 @@ int main()
     BST<string> bt; // = BST<string>("test");
     string addStr = "t"; //est";
     bt.add(addStr);
-    addStr = "z";
-    bt.add(addStr);
+    // addStr = "z";
+    // bt.add(addStr);
     return 0;
 }
