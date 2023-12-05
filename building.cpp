1e4ea88ffe5c957ece942ca5b67eb69a690be4c8
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-05 15:59:41 -0800

    Getting support with better error codes to figure out the problem

--- building.cpp
+++ building.cpp
@@ -6,6 +6,10 @@ int main()
     BST<string> bt; // = BST<string>("test");
     string addStr = "t"; //est";
     bt.add(addStr);
+
+    //copy constructor usage (to help with debugging the CC)
+	BST<string> B4(bt);
+
     // addStr = "z";
     // bt.add(addStr);
     return 0;
