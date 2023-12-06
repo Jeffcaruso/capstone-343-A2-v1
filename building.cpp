fc1523fde41736f12f629da360faca5ace2a7f81
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-06 13:59:31 -0800

    Some more changes *SD*
    Will do a final review looking for any more changes. Then after that, split this into solution branch, start working on starter code branch.

--- building.cpp
+++ building.cpp
@@ -1,3 +1,12 @@
+//============================================================================
+// Name        : Building
+// Test Desc.  : For building in the build section.
+//                  Also can have calls made for 'testing' within this, and then you can see -g information.
+// Author      : Jeffrey Caruso, Yusuf Pisan
+// Date    	   : Fall 2023
+//============================================================================
+
+
 #include "applib/bst.h" 
 #include <string>
 
