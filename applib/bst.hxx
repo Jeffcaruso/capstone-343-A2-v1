e68441ddd5e38bd9f3b403023eb78412d4217455
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-05 16:36:40 -0800

    see if this helps

--- applib/bst.hxx
+++ applib/bst.hxx
@@ -401,6 +401,7 @@ void rCC(Node **copyThis, Node **newTree)
 	return rCC(&((*copyThis)->Left), &((*newTree)->Left));
 	return rCC(&((*copyThis)->Right), &((*newTree)->Right));
 
+	return;
 	//return *newTree;
 }
 
